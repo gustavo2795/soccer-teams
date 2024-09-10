4dec0290be8217ffe182d3a8c3172820d5f4858a
gustavo2795 <gustavo.unb27@gmail.com>
2024-09-09 21:01:01 -0300

    Initial Commit

---
+++ Apple-frameworks/Apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_frameworksApp.swift
+//  Apple-frameworks
+//
+//  Created by Gustavo Braz on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
